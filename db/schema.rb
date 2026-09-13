22e5aa890c53596d7d9485008cdf8acf58e31c0d
Hussain <mhussainabid2006@gmail.com>
2026-09-14 00:12:13 +0500

    pr fb: address day1 PR review feedback

--- db/schema.rb
+++ db/schema.rb
@@ -83,8 +83,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_01_180800) do
 
   create_table "plans", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.decimal "monthly_fee"
-    t.string "name"
+    t.decimal "monthly_fee", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
@@ -116,7 +116,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_01_180800) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.decimal "amount"
+    t.decimal "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "occurred_at"
     t.string "status"
