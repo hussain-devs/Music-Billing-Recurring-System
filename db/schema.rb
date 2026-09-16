973795f77761ea905fb3d902602c18ab79ad34e6
Hussain <mhussainabid2006@gmail.com>
2026-09-16 20:43:52 +0500

    feat(profile): add buyer and admin profile offcanvas

--- db/schema.rb
+++ db/schema.rb
@@ -154,6 +154,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_14_183500) do
     t.string "reset_password_token"
     t.integer "role_id"
     t.datetime "updated_at", null: false
+    t.text "about"
+    t.text "interests"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
